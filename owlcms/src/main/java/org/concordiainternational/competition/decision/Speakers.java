e662eaee3f799a7145ce7a4d52f7b6d640c86c81
lamyjeanfrancois <lamyjeanfrancois@localhost>
2011-07-19 15:04:15 -0400

    Moved sound generation to server.

--- owlcms/src/main/java/org/concordiainternational/competition/decision/Speakers.java
+++ owlcms/src/main/java/org/concordiainternational/competition/decision/Speakers.java
@@ -1,7 +1,11 @@
 package org.concordiainternational.competition.decision;
 
+import java.io.InputStream;
+
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
@@ -29,17 +33,45 @@ public class Speakers {
 				if (mixer != defaultMixer && ! mixer.getMixerInfo().toString().startsWith("Java")) {
 					AudioFormat af = new AudioFormat(8000f,8,1,true,false);
 					if (AudioSystem.getSourceDataLine(af,info) != null) {
-						//new Tone(mixer, 1100, 1200, 1.0).emit();
+						testSound(mixer);
 						System.out.println("**** ok: " + info.toString());
+						Thread.sleep(2000);
 					}					
 				}
 			} catch (IllegalArgumentException e) {
 				System.out.println("not ok: " + info.toString()+" "+e);
 			} catch (LineUnavailableException e) {
 				System.out.println("not ok: " + info.toString()+" "+e);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
+
+	/**
+	 * @param mixer
+	 */
+	protected static void testSound(Mixer mixer) {
+		//new Tone(mixer, 1100, 1200, 1.0).emit();
+        try {
+        	// must use wav format; did not find easy way to get mp3spi to work.
+            final InputStream resource = Speakers.class.getResourceAsStream("/sounds/initialWarning.wav");
+			AudioInputStream inputStream = AudioSystem.getAudioInputStream(resource);
+			
+	        try {
+	            Clip clip = AudioSystem.getClip(mixer.getMixerInfo());
+	            clip.open(inputStream);
+	            clip.start(); 
+	          } catch (Exception e) {
+	            System.err.println(e.getMessage());
+	          }
+			    
+          } catch (Exception e) {
+            System.err.println(e.getMessage());
+          }
+	}
+	
 
 	/**
 	 * @param infos
