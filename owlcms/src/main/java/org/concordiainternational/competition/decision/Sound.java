f873b6c99e1c2d3caaa9a6929b2a3c0786ce5b8d
lamyjeanfrancois <lamyjeanfrancois@localhost>
2012-01-28 16:20:05 -0500

    fixed the way sounds emitted to avoid creating dead threads.

--- owlcms/src/main/java/org/concordiainternational/competition/decision/Sound.java
+++ owlcms/src/main/java/org/concordiainternational/competition/decision/Sound.java
@@ -5,30 +5,51 @@ import java.io.InputStream;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 import javax.sound.sampled.Mixer;
 
+/**
+ * Play a sampled sound.
+ * Requires an uncompressed format (WAV), not a compressed (MP3) format.
+ * 
+ * @author jflamy
+ */
 public class Sound {
 	Mixer mixer;
 	private InputStream resource;
 
-	
+
 	public Sound(Mixer mixer, String soundRelativeURL) throws IllegalArgumentException {
 		this.mixer = mixer;
 		this.resource = Sound.class.getResourceAsStream(soundRelativeURL);
 	}
 
-	
-	public void emit() {
-        try {
-        	if (mixer == null) return;
-			// must use wav format
-        	AudioInputStream inputStream = AudioSystem.getAudioInputStream(resource);
-        	Clip clip = AudioSystem.getClip(mixer.getMixerInfo());
-        	clip.open(inputStream);
-        	clip.start(); 			    
-          } catch (Exception e) {
-        	  throw new RuntimeException(e);
-          }
+
+	public synchronized void emit() {
+		try {
+			if (mixer == null) return;
+			
+			final AudioInputStream inputStream = AudioSystem.getAudioInputStream(resource);
+			final Clip clip = AudioSystem.getClip(mixer.getMixerInfo());
+			clip.open(inputStream);
+			
+			// clip.start() creates a native thread 'behind the scenes'
+			// unless this is added, it never goes away
+			// ref: http://stackoverflow.com/questions/837974/determine-when-to-close-a-sound-playing-thread-in-java
+			clip.addLineListener( new LineListener() {
+				@Override
+				public void update(LineEvent evt) {
+					if (evt.getType() == LineEvent.Type.STOP) {
+						evt.getLine().close();
+					}
+				}
+			});
+			clip.start();
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
