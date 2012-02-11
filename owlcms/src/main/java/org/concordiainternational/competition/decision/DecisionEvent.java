6a542120c85c358009b910e0f189427b0c675774
lamyjeanfrancois <lamyjeanfrancois@localhost>
2012-02-11 18:33:11 -0500

    Updated Licence to MPL-2.0

--- owlcms/src/main/java/org/concordiainternational/competition/decision/DecisionEvent.java
+++ owlcms/src/main/java/org/concordiainternational/competition/decision/DecisionEvent.java
@@ -1,19 +1,10 @@
-/* 
- * Copyright ©2009 Jean-François Lamy
+/*
+ * Copyright 2009-2012, Jean-François Lamy
  * 
- * Licensed under the Open Software Licence, Version 3.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.opensource.org/licenses/osl-3.0.php
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
  */
-
 package org.concordiainternational.competition.decision;
 
 import java.util.EventObject;
