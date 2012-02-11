74b50d8ec2a50e433465340d5ad4d2d5f2cd89c9
lamyjeanfrancois <lamyjeanfrancois@localhost>
2012-02-11 18:18:12 -0500

    Updated Licence to MPL-2.0

--- touchdiv/src/main/java/org/vaadin/touchdiv/TouchEventType.java
+++ touchdiv/src/main/java/org/vaadin/touchdiv/TouchEventType.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2009-2012, Jean-François Lamy
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, 
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.vaadin.touchdiv;
 
 public enum TouchEventType {
