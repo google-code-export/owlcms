56f1f95e191a9a9638c330a2024c614217c9ae45
lamyjeanfrancois <lamyjeanfrancois@localhost>
2012-03-02 06:56:37 -0500

    Support CSV import.

--- owlcms/src/main/java/org/concordiainternational/competition/spreadsheet/SpreadsheetUploader.java
+++ owlcms/src/main/java/org/concordiainternational/competition/spreadsheet/SpreadsheetUploader.java
@@ -9,14 +9,24 @@ package org.concordiainternational.competition.spreadsheet;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URL;
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.servlet.ServletContext;
 
+import org.concordiainternational.competition.data.Category;
+import org.concordiainternational.competition.data.CategoryLookup;
+import org.concordiainternational.competition.data.CompetitionSession;
 import org.concordiainternational.competition.data.Lifter;
 import org.concordiainternational.competition.data.RuleViolationException;
 import org.concordiainternational.competition.i18n.Messages;
@@ -25,6 +35,13 @@ import org.concordiainternational.competition.ui.components.ApplicationView;
 import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.ParseDate;
+import org.supercsv.cellprocessor.ParseInt;
+import org.supercsv.cellprocessor.constraint.IsIncludedIn;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.prefs.CsvPreference;
 
 import com.vaadin.terminal.DownloadStream;
 import com.vaadin.terminal.FileResource;
@@ -42,162 +59,212 @@ import com.vaadin.ui.Window.CloseListener;
  * 
  */
 public class SpreadsheetUploader extends CustomComponent implements Upload.SucceededListener, Upload.FailedListener,
-        Upload.Receiver, ApplicationView {
-    private static final long serialVersionUID = 6843262937708809785L;
-    final private static Logger logger = LoggerFactory.getLogger(SpreadsheetUploader.class);
+Upload.Receiver, ApplicationView {
+	private static final long serialVersionUID = 6843262937708809785L;
+	final private static Logger logger = LoggerFactory.getLogger(SpreadsheetUploader.class);
 
-    Panel root; // Root element for contained components.
-    Panel resultPanel; // Panel that contains the uploaded image.
-    File file; // File to write to.
+	Panel root; // Root element for contained components.
+	Panel resultPanel; // Panel that contains the uploaded image.
+	File file; // File to write to.
 
-    private CompetitionApplication app;
+	private CompetitionApplication app;
 
-    private Label status;
+	private Label status;
 
-    private Locale locale;
-    private String viewName;
+	private Locale locale;
+	private String viewName;
 
-    public SpreadsheetUploader(boolean initFromFragment, String viewName) {
-        if (initFromFragment) {
-            setParametersFromFragment();
-        } else {
-            this.viewName = viewName;
-        }
-        
-        this.app = CompetitionApplication.getCurrent();
-        this.locale = app.getLocale();
+	public SpreadsheetUploader(boolean initFromFragment, String viewName) {
+		if (initFromFragment) {
+			setParametersFromFragment();
+		} else {
+			this.viewName = viewName;
+		}
 
-        root = new Panel(Messages.getString("SpreadsheetUploader.SpreadsheetUpload", locale)); //$NON-NLS-1$
-        setCompositionRoot(root);
+		this.app = CompetitionApplication.getCurrent();
+		this.locale = app.getLocale();
 
-        // Create the Upload component.
-        //final Upload upload = new Upload(Messages.getString("SpreadsheetUploader.ChooseFile",locale), this); //$NON-NLS-1$
-        final Upload upload = new Upload("", this); //$NON-NLS-1$
-        upload.setImmediate(true); // start immediately as soon as the file is
-                                   // selected.
+		root = new Panel(Messages.getString("SpreadsheetUploader.SpreadsheetUpload", locale)); //$NON-NLS-1$
+		setCompositionRoot(root);
 
-        // Use a custom button caption instead of plain "Upload".
-        upload.setButtonCaption(Messages.getString("SpreadsheetUploader.UploadNow", locale)); //$NON-NLS-1$
+		// Create the Upload component.
+		//final Upload upload = new Upload(Messages.getString("SpreadsheetUploader.ChooseFile",locale), this); //$NON-NLS-1$
+		final Upload upload = new Upload("", this); //$NON-NLS-1$
+		upload.setImmediate(true); // start immediately as soon as the file is
+		// selected.
 
-        // Listen for events regarding the success of upload.
-        upload.addListener((Upload.SucceededListener) this);
-        upload.addListener((Upload.FailedListener) this);
-        root.addComponent(upload);
-        root.addComponent(new Label());
+		// Use a custom button caption instead of plain "Upload".
+		upload.setButtonCaption(Messages.getString("SpreadsheetUploader.UploadNow", locale)); //$NON-NLS-1$
 
-        // Create a panel for displaying the uploaded file.
-        resultPanel = new Panel();
-        status = new Label(Messages.getString("SpreadsheetUploader.NoSpreadsheetUploadedYet", locale)); //$NON-NLS-1$
-        resultPanel.addComponent(status);
-        root.addComponent(resultPanel);
-    }
+		// Listen for events regarding the success of upload.
+		upload.addListener((Upload.SucceededListener) this);
+		upload.addListener((Upload.FailedListener) this);
+		root.addComponent(upload);
+		root.addComponent(new Label());
 
-    // Callback method to begin receiving the upload.
-    @Override
-    public OutputStream receiveUpload(String filename, String MIMEType) {
-        FileOutputStream fos = null; // Output stream to write to
-        try {
-            ServletContext sCtx = app.getServletContext();
-            String dirName = sCtx.getRealPath("registration"); //$NON-NLS-1$
-            File dir = new File(dirName);
-            logger.debug(dir.getAbsolutePath());
+		// Create a panel for displaying the uploaded file.
+		resultPanel = new Panel();
+		status = new Label(Messages.getString("SpreadsheetUploader.NoSpreadsheetUploadedYet", locale)); //$NON-NLS-1$
+		resultPanel.addComponent(status);
+		root.addComponent(resultPanel);
+	}
 
-            if (!dir.exists()) dir.mkdirs();
-            File longPath = new File(filename);
-            filename = longPath.getName();
-            file = new File(dir, filename);
-            logger.debug("writing to {}", file.getAbsolutePath()); //$NON-NLS-1$
+	// Callback method to begin receiving the upload.
+	@Override
+	public OutputStream receiveUpload(String filename, String MIMEType) {
+		FileOutputStream fos = null; // Output stream to write to
+		try {
+			ServletContext sCtx = app.getServletContext();
+			String dirName = sCtx.getRealPath("registration"); //$NON-NLS-1$
+			File dir = new File(dirName);
+			logger.debug(dir.getAbsolutePath());
 
-            // Open the file for writing.
-            fos = new FileOutputStream(file);
-        } catch (final java.io.FileNotFoundException e) {
-            // Error while opening the file. Not reported here.
-            e.printStackTrace();
-            this.setComponentError(new SystemError(e));
-            throw new SystemError(e);
-        }
-        return fos; // Return the output stream to write to
-    }
+			if (!dir.exists()) dir.mkdirs();
+			File longPath = new File(filename);
+			filename = longPath.getName();
+			file = new File(dir, filename);
+			logger.debug("writing to {}", file.getAbsolutePath()); //$NON-NLS-1$
 
-    // This is called if the upload is finished.
-   @Override
-    public void uploadSucceeded(Upload.SucceededEvent event) {
-        // Log the upload on screen.
-        final String messageFormat = Messages.getString("SpreadsheetUploader.Status", locale); //$NON-NLS-1$
-        final String mimeType = event.getMIMEType();
-        status.setValue(MessageFormat.format(messageFormat, event.getFilename(), mimeType));
-        processUploadedFile();
-    }
+			// Open the file for writing.
+			fos = new FileOutputStream(file);
+		} catch (final java.io.FileNotFoundException e) {
+			// Error while opening the file. Not reported here.
+			e.printStackTrace();
+			this.setComponentError(new SystemError(e));
+			throw new SystemError(e);
+		}
+		return fos; // Return the output stream to write to
+	}
 
-    /**
-     * @throws SystemError
-     */
-    private void processUploadedFile() throws SystemError {
-        // process the file
-        logger.debug("reading from: {}", file); //$NON-NLS-1$
-        final FileResource fileResource = new FileResource(file, getApplication());
-        DownloadStream ds = fileResource.getStream();
-        try {
-            final WeighInSheet weighInSheet = new WeighInSheet(app);
-            final Session hbnSession = app.getHbnSession();
-            weighInSheet.readHeader(ds.getStream(), app);
+	// This is called if the upload is finished.
+	@Override
+	public void uploadSucceeded(Upload.SucceededEvent event) {
+		// Log the upload on screen.
+		final String messageFormat = Messages.getString("SpreadsheetUploader.Status", locale); //$NON-NLS-1$
+		final String mimeType = event.getMIMEType();
+		status.setValue(MessageFormat.format(messageFormat, event.getFilename(), mimeType));
+		processUploadedFile(mimeType);
+	}
+
+	/**
+	 * @param mimeType 
+	 * @throws SystemError
+	 */
+	private void processUploadedFile(String mimeType) throws SystemError {
+		// process the file
+		logger.debug("reading from: {}", file); //$NON-NLS-1$
+		final FileResource fileResource = new FileResource(file, getApplication());
+		DownloadStream ds = fileResource.getStream();
+		if (file.getPath().endsWith(".csv")) {
+			processCSV(ds);
+		} else if (mimeType.endsWith("xls") || mimeType.endsWith("excel")) {
+			processXLS(ds);
+		} else {
+			throw new RuntimeException("Unknown format.");
+		}
+
+	}
+
+	private void processCSV(DownloadStream ds) {
+		InputStream is = ds.getStream();
+		CsvBeanReader cbr = new CsvBeanReader(new InputStreamReader(is), CsvPreference.EXCEL_PREFERENCE);
+		List<CompetitionSession> sessionList = CompetitionSession.getAll();
+		Set<Object> sessionNameSet = new TreeSet<Object>();
+		for (CompetitionSession s : sessionList) {
+			sessionNameSet.add(s.getName());
+		}
+		List<Category> categoryList = CategoryLookup.getSharedInstance().getCategories();
+		Set<Object> categoryNameSet = new TreeSet<Object>();
+		for (Category c : categoryList) {
+			categoryNameSet.add(c.getName());
+		}
+		
+		final CellProcessor[] processors = new CellProcessor[] {
+				null, // last name, as is.
+				null, // first name, as is.
+				new IsIncludedIn(new HashSet<Object>(Arrays.asList("M","F"))), // gender
+				null, // club, as is.
+				new ParseDate("yyyy"), // birth year
+				new IsIncludedIn(categoryNameSet), // registrationCategory
+				new IsIncludedIn(sessionNameSet), // sessionName
+				new Optional(new ParseInt()), // registration total
+		};
+		try {
+			final String[] header = cbr.getCSVHeader(true);
+			Lifter lifter;
+			while( (lifter = cbr.read(Lifter.class, header, processors)) != null) {
+				logger.warn("lifter {}", lifter);
+			} 
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				cbr.close();
+			} catch (IOException e) {
+				// ignored
+			}
+		}
+	}
+
+	private void processXLS(DownloadStream ds) {
+		try {
+			final WeighInSheet weighInSheet = new WeighInSheet(app);
+			final Session hbnSession = app.getHbnSession();
+			weighInSheet.readHeader(ds.getStream(), app);
 			List<Lifter> lifters = weighInSheet.getAllLifters(ds.getStream(), app);
-            for (Lifter curLifter : lifters) {
+			for (Lifter curLifter : lifters) {
 				hbnSession.save(curLifter);
-            }
-        } catch (Throwable t) {
-            t.printStackTrace();
-            throw new SystemError(t);
-        }
-        // Display the uploaded file in the panel.
-        // resultPanel.removeAllComponents();
-        // resultPanel.addComponent(new Embedded("", fileResource));
-    }
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+			throw new SystemError(t);
+		}
+	}
 
-    // This is called if the upload fails.
-    @Override
+	// This is called if the upload fails.
+	@Override
 	public void uploadFailed(Upload.FailedEvent event) {
-        // Log the failure on screen.
-        final String messageFormat = Messages.getString("SpreadsheetUploader.UploadingFailure", locale); //$NON-NLS-1$
-        final String mimeType = event.getMIMEType();
-        status.setValue(MessageFormat.format(messageFormat, event.getFilename(), mimeType));
-    }
+		// Log the failure on screen.
+		final String messageFormat = Messages.getString("SpreadsheetUploader.UploadingFailure", locale); //$NON-NLS-1$
+		final String mimeType = event.getMIMEType();
+		status.setValue(MessageFormat.format(messageFormat, event.getFilename(), mimeType));
+	}
 
-    @Override
-    public void refresh() {
-        // nothing to do
-    }
+	@Override
+	public void refresh() {
+		// nothing to do
+	}
 
-    /* (non-Javadoc)
-     * @see org.concordiainternational.competition.ui.components.ApplicationView#needsMenu()
-     */
-    @Override
-    public boolean needsMenu() {
-        return true;
-    }
+	/* (non-Javadoc)
+	 * @see org.concordiainternational.competition.ui.components.ApplicationView#needsMenu()
+	 */
+	@Override
+	public boolean needsMenu() {
+		return true;
+	}
 
-    /**
-     * @return
-     */
-    @Override
+	/**
+	 * @return
+	 */
+	@Override
 	public String getFragment() {
-        return viewName;
-    }
-    
+		return viewName;
+	}
 
-    /* (non-Javadoc)
-     * @see org.concordiainternational.competition.ui.components.ApplicationView#setParametersFromFragment(java.lang.String)
-     */
-    @Override
-    public void setParametersFromFragment() {
-        String frag = CompetitionApplication.getCurrent().getUriFragmentUtility().getFragment();
-        String[] params = frag.split("/");
-        if (params.length >= 1) {
-            viewName = params[0];
-        } else {
-            throw new RuleViolationException("Error.ViewNameIsMissing"); 
-        }
-    }
+
+	/* (non-Javadoc)
+	 * @see org.concordiainternational.competition.ui.components.ApplicationView#setParametersFromFragment(java.lang.String)
+	 */
+	@Override
+	public void setParametersFromFragment() {
+		String frag = CompetitionApplication.getCurrent().getUriFragmentUtility().getFragment();
+		String[] params = frag.split("/");
+		if (params.length >= 1) {
+			viewName = params[0];
+		} else {
+			throw new RuleViolationException("Error.ViewNameIsMissing"); 
+		}
+	}
 
 	@Override
 	public void registerAsListener() {
@@ -208,12 +275,12 @@ public class SpreadsheetUploader extends CustomComponent implements Upload.Succe
 	public void unregisterAsListener() {
 		app.getMainWindow().addListener((CloseListener) this);
 	}
-	
+
 	@Override
 	public void windowClose(CloseEvent e) {
 		unregisterAsListener();	
 	}
-	
+
 	/* Called on refresh.
 	 * @see com.vaadin.terminal.URIHandler#handleURI(java.net.URL, java.lang.String)
 	 */
