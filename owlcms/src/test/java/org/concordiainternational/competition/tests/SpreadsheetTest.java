5892c1c77f3ab1a8d9ce564a576be97a356ad169
lamyjeanfrancois <lamyjeanfrancois@localhost>
2013-03-15 19:54:34 -0400

    removed obsolete classes

--- owlcms/src/test/java/org/concordiainternational/competition/tests/SpreadsheetTest.java
+++ owlcms/src/test/java/org/concordiainternational/competition/tests/SpreadsheetTest.java
@@ -7,25 +7,17 @@
  */
 package org.concordiainternational.competition.tests;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.List;
 
 import org.concordiainternational.competition.data.CategoryLookup;
-import org.concordiainternational.competition.data.CompetitionSession;
 import org.concordiainternational.competition.data.Lifter;
 import org.concordiainternational.competition.spreadsheet.InputSheet;
-import org.concordiainternational.competition.spreadsheet.ResultSheet;
 import org.concordiainternational.competition.spreadsheet.WeighInSheet;
-import org.concordiainternational.competition.ui.CompetitionApplication;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,8 +32,6 @@ public class SpreadsheetTest {
 
     HbnSessionManager hbnSessionManager = AllTests.getSessionManager();
     Logger logger = LoggerFactory.getLogger(SpreadsheetTest.class);
-    static private String loadResults;
-    static private File tempFile;
     static private List<Lifter> lifters;
 
     @Before
@@ -70,24 +60,7 @@ public class SpreadsheetTest {
         loadResults = AllTests.longDump(lifters, false);
     }
 
-    @Ignore
-    public void writeAllLifters() throws Throwable {
-        tempFile = File.createTempFile("myApp", ".xls"); //$NON-NLS-1$ //$NON-NLS-2$
-        //tempFile.deleteOnExit();
-        logger.debug("temporary file is set to "+tempFile.getAbsolutePath());
-        FileOutputStream os = new FileOutputStream(tempFile);
-        new ResultSheet(CategoryLookup.getSharedInstance(), new CompetitionApplication(), (CompetitionSession) null)
-                .writeLifters(lifters, os);
-    }
 
-    @Ignore
-    public void reloadFromOutput() throws Throwable {
-        InputStream is = new FileInputStream(tempFile);
-        ResultSheet readAgain = new ResultSheet(AllTests.getSessionManager());
-        lifters = readAgain.getAllLifters(is, hbnSessionManager);
-        String loadAgainResults = AllTests.longDump(lifters, false);
-        assertEquals("roundtrip failed", loadResults, loadAgainResults); //$NON-NLS-1$
-    }
 
 
 }
