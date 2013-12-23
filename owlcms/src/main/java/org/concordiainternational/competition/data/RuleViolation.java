816c1d53f61cd22fcf3e8b4410fa486e2b64b314
jflamy <lamyjeanfrancois@gmail.com>
2013-12-22 20:28:46 -0500

    flow: Created branch 'feature/tv_colors'.

---
+++ owlcms/src/main/java/org/concordiainternational/competition/data/RuleViolation.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2009-2012, Jean-François Lamy
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ */
+package org.concordiainternational.competition.data;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RuleViolation {
+    final static Logger logger = LoggerFactory.getLogger(RuleViolation.class);
+
+    // public static RuleViolationException change1ValueTooSmall(Object... objs)
+    // {
+    //		return new RuleViolationException(("RuleViolation.change1ValueTooSmall"), objs);   //$NON-NLS-1$
+    // }
+    //
+    // public static RuleViolationException change2ValueTooSmall(Object... objs)
+    // {
+    //		return new RuleViolationException(("RuleViolation.change2ValueTooSmall"), objs);   //$NON-NLS-1$
+    // }
+    //
+    public static RuleViolationException declarationValueTooSmall(Object... objs) {
+        return new RuleViolationException(("RuleViolation.declarationValueTooSmall"), objs); //$NON-NLS-1$
+    }
+
+    //
+    // public static RuleViolationException liftValueTooSmall(Object... objs) {
+    //		return new RuleViolationException(("RuleViolation.liftValueTooSmall"), objs);   //$NON-NLS-1$
+    // }
+
+    public static RuleViolationException liftValueNotWhatWasRequested(Object... objs) {
+        return new RuleViolationException(("RuleViolation.liftValueNotWhatWasRequested"), objs); //$NON-NLS-1$
+    }
+
+    public static RuleViolationException declaredChangesNotOk(Object... objs) {
+        return new RuleViolationException(("RuleViolation.declaredChangesNotOk"), objs); //$NON-NLS-1$
+    }
+
+    public static RuleViolationException liftValueBelowProgression(int curLift, String actualLift,
+            int automaticProgression) {
+        return new RuleViolationException(
+                ("RuleViolation.liftValueBelowProgression"), curLift, actualLift, automaticProgression); //$NON-NLS-1$
+    }
+
+    public static RuleViolationException rule15_20Violated(Object... objs) {
+        return new RuleViolationException("RuleViolation.rule15_20Violated", objs); //$NON-NLS-1$
+    }
+
+}
