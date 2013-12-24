04fb20ef72af822f94ead3f4eb8fc16e74ee4955
lamyjeanfrancois <lamyjeanfrancois@localhost>
2013-12-24 14:30:18 -0500

    owlcms.locale system property is used to override browser locales.

--- owlcms/src/main/java/org/concordiainternational/competition/i18n/LocalizedApplication.java
+++ owlcms/src/main/java/org/concordiainternational/competition/i18n/LocalizedApplication.java
@@ -28,7 +28,7 @@ public class LocalizedApplication extends Application {
         // is running.
         LocalizedSystemMessages msg = (LocalizedSystemMessages) getSystemMessages();
         msg.setThreadLocale(this.getLocale()); // by default getLocale() comes
-                                               // from the user's browser.
+        // from the user's browser.
 
         // the following defines what will happen before and after each http
         // request.
@@ -52,11 +52,11 @@ public class LocalizedApplication extends Application {
     public static SystemMessages getSystemMessages() {
         if (localizedMessages == null)
             localizedMessages = new LocalizedSystemMessages() {
-                @Override
-                protected Locale getDefaultSystemMessageLocale() {
-                    return Locale.CANADA_FRENCH;
-                }
-            };
+            @Override
+            protected Locale getDefaultSystemMessageLocale() {
+                return Locale.CANADA_FRENCH;
+            }
+        };
         return localizedMessages;
     }
 
@@ -79,6 +79,48 @@ public class LocalizedApplication extends Application {
                 ((LocalizedSystemMessages) getSystemMessages()).setThreadLocale(getLocale());
             }
         });
+    }
+
+    /**
+     * Convert a string based locale into a Locale Object. Assumes the string has form "{language}_{country}_{variant}". Examples: "en",
+     * "de_DE", "_GB", "en_US_WIN", "de__POSIX", "fr_MAC"
+     * 
+     * @param localeString
+     *            The String
+     * @return the Locale
+     */
+    public static Locale getLocaleFromString(String localeString) {
+        if (localeString == null) {
+            return null;
+        }
+        localeString = localeString.trim();
+        if (localeString.toLowerCase().equals("default")) {
+            return Locale.getDefault();
+        }
+
+        // Extract language
+        int languageIndex = localeString.indexOf('_');
+        String language = null;
+        if (languageIndex == -1) {
+            // No further "_" so is "{language}" only
+            return new Locale(localeString, "");
+        } else {
+            language = localeString.substring(0, languageIndex);
+        }
+
+        // Extract country
+        int countryIndex = localeString.indexOf('_', languageIndex + 1);
+        String country = null;
+        if (countryIndex == -1) {
+            // No further "_" so is "{language}_{country}"
+            country = localeString.substring(languageIndex + 1);
+            return new Locale(language, country);
+        } else {
+            // Assume all remaining is the variant so is "{language}_{country}_{variant}"
+            country = localeString.substring(languageIndex + 1, countryIndex);
+            String variant = localeString.substring(countryIndex + 1);
+            return new Locale(language, country, variant);
+        }
     }
 
 }
